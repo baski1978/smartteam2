d8a38a13bcf7da17b39a5dc27ee2c486c695fb69
Namile <bnamile@ms.ds.uhc.com>
2020-02-18 12:31:33 +0530

    passing list to template

--- site_smartteam/loadfile/views.py
+++ site_smartteam/loadfile/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'empdetails.html')
+	values[] = getValues('empdetails.csv')
+    return render(request, 'empdetails.html',{'emplist':values[]})
 
 
+def getValues(filename):
+	try:
+		file=open(filename,'r')
+	except IOError:
+		print 'problem with file' , filename
 
+	values = []
+	for line in file:
+		values.append(line)
+	return values
