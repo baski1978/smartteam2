54ebf2a65d121a2c27318b11b9b81e61287ee8c5
Namile <bnamile@ms.ds.uhc.com>
2020-02-18 16:34:17 +0530

    imports related to filter refistery

--- site_smartteam/loadfile/views.py
+++ site_smartteam/loadfile/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 import os
 from django.conf import settings
 import sys
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
 
 @register.filter(name='cut')
 def splits(value,arg)
