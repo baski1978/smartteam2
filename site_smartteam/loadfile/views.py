bcb7ef36ddd4ca020ecaab4ed54cc8e3de1d0c6f
Namile <bnamile@ms.ds.uhc.com>
2020-02-18 15:17:01 +0530

    adding file path and exception handlers

--- site_smartteam/loadfile/views.py
+++ site_smartteam/loadfile/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import os
+from django.conf import settings
+import sys
 
 # Create your views here.
 def home(request):
     values = []
-    values = getValues('empdetails.csv')
+    values = getValues('/home/nsrivin/dir_smartteam/env_smartteam/site_smartteam/loadfile/empdetails.csv')
     return render(request, 'empdetails.html',{'emplist':values})
 
-
 def getValues(filename):
-    file=open(filename,'r')
     values = []
-    for line in file:
-        values.append(line)
-    return values
+	try:
+	    file=open(filename,'r')
+	    for line in file:
+		    values.append(line)
+		return values
+	except OSError as err:
+		print("OS error: {0}".format(err))
+	except ValueError:
+		print("Could not convert data to an integer.")
+	except:
+	    print("Unexpected error:", sys.exc_info()[0])
+		raise
