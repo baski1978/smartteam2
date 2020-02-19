872f4f6a3a44d4c2ec30a5c9b83ea1eaebb15c3e
Namile <bhaskar_namile@optum.com>
2020-02-19 17:53:25 +0530

    adasdasd

--- site_smartteam/loadfile/urls.py
+++ site_smartteam/loadfile/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home' ),
-	path('',views.projectteam, name='projectteam' )
+	path('projectteams',views.projectteams, name='projectteams' )
 ]
 
 projectteams
