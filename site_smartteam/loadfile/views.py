1a575d832189a5bd7230f5803da0da83cf4823fa
baski1978 <bhaskar.namile@gmail.com>
2020-03-28 13:37:19 +0530

    commitn

--- site_smartteam/loadfile/views.py
+++ site_smartteam/loadfile/views.py
@@ -10,9 +10,6 @@ from loadfile.models import IndConsidered
 from loadfile.models import Prjnumbers
 from loadfile.models import TempTeam
 
-
-# Create your views here.
-########### em details Page#####################
 def home(request):
 	IndConsidered.objects.all().delete()
 	return render(request, 'index.html')
@@ -22,71 +19,68 @@ def empdetails(request):
 	clos =[]
 	rowsx = []
 	
-	#values = getValues(CSVFILES_FOLDER+'empdetails.csv')
-	#Individuals.objects.all().delete()
+	values = getValues(CSVFILES_FOLDER+'empdetails.csv') 
+	Individuals.objects.all().delete()
 	IndConsidered.objects.all().delete()
 	TempTeam.objects.all().delete()
 	Prjnumbers.objects.all().delete()
-	#for rowsx in values:
-	#	cols = rowsx.split(",")
-	#	CreateInd(cols)
+	for rowsx in values:
+		cols = rowsx.split(",")
+		CreateInd(cols)
 	fint=Individuals.objects.filter(indTname='bench').all()	
 	return render(request, 'empdetails2.html',{'emplist':values,'ctrec':countofrecords(),'data_emp':fint })
 
-	
+def CommitTeam(request,prjno):
 
+	IndConsidered.objects.all().delete()
+	TempTeam.objects.all().delete()
+	Prjnumbers.objects.all().delete()
+	Id=TempTeam.objects.values_list('indId',flat=True)
 
-########### Render the project team page #####################
+	for rowsx in values:
+		cols = rowsx.split(",")
+		CreateInd(cols)
+	fint=Individuals.objects.filter(indTname='bench').all()	
+	return render(request, 'empdetails2.html',{'emplist':values,'ctrec':countofrecords(),'data_emp':fint })
+
 
 def projectteams(request, indIdno):
 	
-	TempTeam.objects.all().delete()
-	populatetemtable2(10)
-#	fitvalue=TempTeam.objects.values_list('tFitnessValue',flat=True)[:1]						#Step2: Check fitvalue	
-	
-#	while fitvalue != 'fit':
-	crossover2()																				#step3: Do Crossover for none28								
-#	mutation()																					#Step4: Do Mutation for 28
-#	fitvalue=TempTeam.objects.values_list('tFitnessValue',flat=True)[:1]						#Step6: Check fit value
-	fint=TempTeam.objects.all()																	#step7: Get project details
+	if indIdno==0:
+		TempTeam.objects.all().delete()
+		IndConsidered.objects.all().delete()
+		a=[]
+		prjno=populatetemtable2(10,a,'')
+
+	fitvalue=str(TempTeam.objects.values_list('tFitnessValue',flat=True)[:1]).replace("<QuerySet ['","").replace("']>","")
+	popularskillids , popularskill = mostpopularskill()
+	logging.warning("fitvalue,fitvalue,fitvalue,fitvalue,fitvalue,fitvalue"+str(fitvalue))
+
+	crossover2(popularskillids,popularskill,prjno)									  			    #step3: Do Crossover & mutation 
+	fitvalue=TempTeam.objects.values_list('tFitnessValue',flat=True)[:1]
+
+	fint=TempTeam.objects.all().order_by('indId')																#step7: Get project details
 	currentteamids=TempTeam.objects.values_list('indId',flat=True)            
-	indIdnodetails=Individuals.objects.filter(Q(indId__in=currentteamids)).all()				#step8: Get Individual details
+	indIdnodetails=Individuals.objects.filter(Q(indId__in=currentteamids)).all().order_by('indId','indGrade')				#step8: Get Individual details
 	return render(request, 'projectteams.html',{'data_prj':fint , 'data_ind':indIdnodetails})	#Step9: Display
 		
-def getteamindividuals_28(cnt,popularskillvalue):
-		cursor = connection.cursor()
-		if popularskillvalue=="":
-			cursor.execute( "select indId from  loadfile_Individuals where indGrade =='28'  and indId not in ( select indId from  loadfile_IndConsidered)")
-		else:
-			qstr="select indId from  loadfile_Individuals where indGrade =='28' and indSkill='" + str(popularskillvalue).replace("<QuerySet ['","").replace("']>","") + "' and indId not in ( select indId from  loadfile_IndConsidered)"
-			cursor.execute(qstr)
-		row = cursor.fetchall()
-		row=str(row).replace("[","").replace("]","").replace("(","").replace(",)","").split(",")
-		cursor.close()
-		randlist = random.sample(row, cnt)
-		return randlist
+def	populatetemtable2(cnt,crossoverIds,prjno):																#Step1: Pick random teams
 
-def getteamindividuals_n28(cnt,popularskillvalue):
-		cursor = connection.cursor()
-		if popularskillvalue=="":
-			cursor.execute( "select indId from  loadfile_Individuals where indGrade !='28' and indId not in ( select indId from  loadfile_IndConsidered)")
-		else:
-			qstr="select indId from  loadfile_Individuals where indGrade !='28' and indSkill='" + str(popularskillvalue).replace("<QuerySet ['","").replace("']>","") + "' and indId not in ( select indId from  loadfile_IndConsidered)"
-			logging.warning(qstr)
-			cursor.execute(qstr)
-		row = cursor.fetchall()
-		row=str(row).replace("[","").replace("]","").replace("(","").replace(",)","").split(",")
-		cursor.close()
-		randlist = random.sample(row, cnt)
-		return randlist
-def	populatetemtable2(cnt):																#Step1: Pick random teams
-	ids28=getteamindividuals_28(2,"")
-	idsn28=getteamindividuals_n28(8,"")
-	tempteamids=ids28+idsn28
-	prjno=Prjnumbers()
-	prjno.tag='prj'
-	prjno.save()
-	projectnumber = Prjnumbers.objects.values_list('prjID', flat=True).last()
+	if cnt==5000:		
+		currentteamids=TempTeam.objects.values_list('indId',flat=True)  
+		currentteamids=str(currentteamids).replace("<QuerySet [","").replace("]>","").split(",") #replace("[","").replace("]","").replace("(","").replace(",)","").split(",")
+		tempteamids=crossoverIds+currentteamids
+	else:
+		ids28=getteamindividuals_28(2,"")
+		idsn28=getteamindividuals_n28(8,"")
+		tempteamids=ids28+idsn28
+	if prjno=='':
+		prjno=Prjnumbers()
+		prjno.tag='prj'
+		prjno.save()
+		projectnumber = Prjnumbers.objects.values_list('prjID', flat=True).last()
+	else:
+	   projectnumber=prjno
 
 	tdevopsRatio=gettdevopsRatio(tempteamids)
 	tdesignRatio=gettdesignRatio(tempteamids)
@@ -98,6 +92,7 @@ def	populatetemtable2(cnt):																#Step1: Pick random teams
 	tCntdistinctskills=gettCntdistinctskills(tempteamids)
 	tFitnessValue= gettFitnessValue(tdevopsRatio,tdesignRatio,tavgCost,tOnOffRatio,\
 								 tratioGtAvgExp,tAvgNoOfPto,tAvgSkillLevel,tCntdistinctskills,)
+	TempTeam.objects.all().delete()
 	for x in tempteamids:
 		temp=TempTeam()
 		temp.tname=projectnumber
@@ -112,35 +107,94 @@ def	populatetemtable2(cnt):																#Step1: Pick random teams
 		temp.tCntdistinctskills  = tCntdistinctskills
 		temp.tFitnessValue  = tFitnessValue 	
 		temp.save()
+	return projectnumber	
+
 
 ################ crossover defination #####################
-def crossover2():
+def crossover2(popularskillIds,popularskillvalue,prjno):	
+	cursor = connection.cursor()
+	if popularskillvalue=="":
+		return
+	else:
+		qstr="select indId from  loadfile_Individuals where IndGrade!='28' and ( indSkill != 'fullstack' and indSkill !='" + str(popularskillvalue).replace("<QuerySet ['","").replace("']>","") + "' ) and indId  in ( select indId from  loadfile_TempTeam)"
+		cursor.execute(qstr)
+		row = cursor.fetchall()
+		row=str(row).replace("[","").replace("]","").replace("(","").replace(",)","").split(",")
+		cursor.close()
+		if str(row)=='':
+			return
+		else:	
+			ct=indConsidered(row)
+			crossoverIds=getteamindividuals_n28(ct,popularskillvalue)
+			populatetemtable2(5000,crossoverIds,prjno)
+	#logging.warning("crossoverIdscrossoverIdscrossoverIdscrossoverIds:"+str(crossoverIds))
+	mutation2(popularskillvalue,prjno)
+	pass
+
+def mutation2(popularskillvalue,prjno):
+	cursor = connection.cursor()
+	qstr="select indId from  loadfile_Individuals where indGrade ='28' and ( indSkill != 'fullstack' and indSkill!='" + str(popularskillvalue).replace("<QuerySet ['","").replace("']>","") + "' ) and indId  in ( select indId from  loadfile_TempTeam)"
+	cursor.execute(qstr)	
+	row=cursor.fetchall()
+	row=str(row).replace("[","").replace("]","").replace("(","").replace(",)","").split(",")
+	cursor.close()
+	if str(row)=='':
+		return
+	else:
+		#logging.warning("mutation2mutation2mutation2mutation2mutation2:"+str(row))
+		ct=indConsidered(row)
+		MutionIds=getteamindividuals_28(ct,popularskillvalue)
+		populatetemtable2(5000,MutionIds,prjno)
+
+def mostpopularskill():
 	Id=TempTeam.objects.values_list('indId',flat=True)
 	most_common = Individuals.objects.filter(indId__in=Id).filter(~Q(indRole='domain')).annotate(mc=Count('indSkill')).order_by('-mc')[:1]
 	mostcommonskill= most_common.values_list('indSkill',flat=True)
-	popularskillIds = Individuals.objects.filter(indId__in=Id).filter(indSkill__in=mostcommonskill).values_list('indId',flat=True)
-	popularskillvalue=Individuals.objects.filter(Q(indId__in = popularskillIds)).values_list('indSkill',flat=True)[:1]
-	indsConsidered=TempTeam.objects.exclude(indId__in=popularskillIds).values_list('indId',flat=True)
-	for x in indsConsidered:
+	popularskillIds = Individuals.objects.filter(Q(indId__in=Id)).filter(Q(indSkill__in=mostcommonskill)).values_list('indId',flat=True)
+	popularskillvalue=Individuals.objects.filter(Q(indId__in = popularskillIds)).values_list('indSkill',flat=True)[:1]	
+	return popularskillIds , popularskillvalue
+	
+def get28ids():
+	g28Ids=Individuals.objects.filter(indGrade='28').values_list('indId',flat=True)
+	tem28id=TempTeam.objects.filter(indId__in=g28Ids).values_list('indId',flat=True)
+	return tem28id
+
+def indConsidered(indsConsidered):
+	try:
+		ct=0
+		#logging.warning("uuuuuuuuuuuuuuuuuuuuuuuu:llllllllll:"+str(indsConsidered))
+		for x in indsConsidered:
 			prjno=IndConsidered()
 			prjno.indId=x
 			prjno.save()
-	TempTeam.objects.filter(Q(indId__in=indsConsidered)).delete()
-	ct = TempTeam.objects.count()
-	crossoverIds=getteamindividuals_n28(10-ct,popularskillvalue)
-	populatetemtable(crossoverIds)
-	mutation2(popularskillvalue,Id)
+			ct=ct+1
+		TempTeam.objects.filter(Q(indId__in=indsConsidered)).delete()
+		return ct
+	except:
+		return 0
+def getteamindividuals_28(cnt,popularskillvalue):
+		cursor = connection.cursor()
+		if popularskillvalue=="":
+			cursor.execute( "select indId from  loadfile_Individuals where indGrade =='28'  and indId not in ( select indId from  loadfile_IndConsidered)")
+		else:
+			qstr="select indId from  loadfile_Individuals where indGrade =='28' and ( indSkill = 'fullstack' or indSkill='" + str(popularskillvalue).replace("<QuerySet ['","").replace("']>","") + "' ) and indId not in ( select indId from  loadfile_IndConsidered)"
+			cursor.execute(qstr)
+		row = cursor.fetchall()
+		row=str(row).replace("[","").replace("]","").replace("(","").replace(",)","").replace(" ","").split(",")
+		cursor.close()
+		randlist = random.sample(row, cnt)
+		return randlist
 
-def mutation2(popularskillvalue,Id):
-
-	Ids = Individuals.objects.filter(indId__in=Id).filter('indGrade'=='28' ).filter('indSkill'!=popularskillvalue).filter('indSkill'!='fullstack')
-	indsConsidered=TempTeam.objects.exclude(indId__in=Ids).values_list('indId',flat=True)
-	ct=0
-	for x in indsConsidered:
-		prjno=IndConsidered()
-		prjno.indId=x
-		prjno.save()
-		ct=ct+1
-	TempTeam.objects.filter(Q(indId__in=indsConsidered)).delete()
-	MutionIds=getteamindividuals_28(ct,popularskillvalue)
-	populatetemtable(MutionIds)
+def getteamindividuals_n28(cnt,popularskillvalue):
+		cursor = connection.cursor()
+		if popularskillvalue=="":
+			cursor.execute( "select indId from  loadfile_Individuals where indGrade !='28' and indId not in ( select indId from  loadfile_IndConsidered)")
+		else:
+			qstr="select indId from  loadfile_Individuals where indGrade !='28' and indSkill='" + str(popularskillvalue).replace("<QuerySet ['","").replace("']>","") + "' and indId not in ( select indId from  loadfile_IndConsidered)"
+			#logging.warning(str(popularskillvalue)+"MMMMMMMMMMMMMMMMMASDSADASDERRWRWEREWRWER")
+			cursor.execute(qstr)
+		row = cursor.fetchall()
+		row=str(row).replace("[","").replace("]","").replace("(","").replace(",)","").split(",")
+		cursor.close()
+		randlist = random.sample(row, cnt)
+		return randlist
--- site_smartteam/utils/functions2.py
+++ site_smartteam/utils/functions2.py
@@ -17,99 +17,99 @@ import random
 from site_smartteam.settings import CSVFILES_FOLDER
 from django.db.models import Q
 
-def justcheck(a):
-    return a
+#def justcheck(a):
+#    return a
 ########### select random ten people for project #####################
 
-def getteamindividuals(cnt , skillfilt):
+#def getteamindividuals(cnt , skillfilt):
+#
+#
+#	randlist = []
+#	randlist3 = []
+#	randlist4 = []
+#
+#	excludids=IndConsidered.objects.values_list('indId',flat=True)
+#	if skillfilt=='':
+#		randlist = list(Individuals.objects.exclude(Q(indId__in=excludids)).values_list('indId',flat=True))
+#	else:
+#		randlist = Individuals.objects.exclude(Q(indId__in=excludids)).values_list('indId',flat=True)
+#		randlist = list(randlist.filter(Q(indSkill__in =list(skillfilt) )))
+#		logging.warning("randlist :oooo: " + str(skillfilt))
 
-
-	randlist = []
-	randlist3 = []
-	randlist4 = []
-
-	excludids=IndConsidered.objects.values_list('indId',flat=True)
-	if skillfilt=='':
-		randlist = list(Individuals.objects.exclude(Q(indId__in=excludids)).values_list('indId',flat=True))
-	else:
-		randlist = Individuals.objects.exclude(Q(indId__in=excludids)).values_list('indId',flat=True)
-		randlist = list(randlist.filter(Q(indSkill__in =list(skillfilt) )))
-		logging.warning("randlist :oooo: " + str(skillfilt))
-
-	randlist2 = random.sample(randlist, cnt)
-	x= justcheck(1)
-
-	for i in randlist2:
-		randlist3.append("".join(str(i)).replace(",)",""))
-	for i in randlist3:
-		randlist4.append("".join(str(i)).replace("(",""))
-
-	return list(randlist4)
-
+#	randlist2 = random.sample(randlist, cnt)
+#	x= justcheck(1)
+#
+#	for i in randlist2:
+#		randlist3.append("".join(str(i)).replace(",)",""))
+#	for i in randlist3:
+#		randlist4.append("".join(str(i)).replace("(",""))
+#
+#	return list(randlist4)
+#
 ########### crossover defination #####################
-def crossover():
-	Id=TempTeam.objects.values_list('indId',flat=True)
-	popularskillIds = getPopularSkill(Id)
-	popularskillvalue=Individuals.objects.filter(Q(indId__in = popularskillIds)).values_list('indSkill',flat=True)[:1]
-	logging.warning("popularskillvalue is $$$$$$$***#### : " + str(popularskillIds) + ":" + str(popularskillvalue) + ":" + str(Id))
-	indsConsidered=TempTeam.objects.exclude(indId__in=popularskillIds).values_list('indId',flat=True)
-	for x in indsConsidered:
-			prjno=IndConsidered()
-			prjno.indId=x
-			prjno.save()
-
-	TempTeam.objects.filter(Q(indId__in=indsConsidered)).delete()
-	ct = TempTeam.objects.count()
-	crossoverIds=getteamindividuals(10-ct,popularskillvalue)
-	populatetemtable(crossoverIds)
+#def crossover():
+#	Id=TempTeam.objects.values_list('indId',flat=True)
+#	popularskillIds = getPopularSkill(Id)
+#	popularskillvalue=Individuals.objects.filter(Q(indId__in = popularskillIds)).values_list('indSkill',flat=True)[:1]
+#	logging.warning("popularskillvalue is $$$$$$$***#### : " + str(popularskillIds) + ":" + str(popularskillvalue) + ":" + str(Id))
+#	indsConsidered=TempTeam.objects.exclude(indId__in=popularskillIds).values_list('indId',flat=True)
+#	for x in indsConsidered:
+#			prjno=IndConsidered()
+#			prjno.indId=x
+#			prjno.save()
+#
+#	TempTeam.objects.filter(Q(indId__in=indsConsidered)).delete()
+#	ct = TempTeam.objects.count()
+#	crossoverIds=getteamindividuals(10-ct,popularskillvalue)
+#	populatetemtable(crossoverIds)
 
 ########### mutation defination #####################
 
-def mutation():
-	
-	tempids = TempTeam.objects.values_list('indId',flat=True)
-	ind28Ids = Individuals.objects.filter(Q(indGrade=28)).filter(indId__in=tempids).values_list('indId',flat=True)
-	if len(ind28Ids)>2:
-		for x in ind28Ids[2:]:
-			prjno=IndConsidered()
-			prjno.indId=x
-			prjno.save()
-		mutationIds=getteamindividuals(len(ind28Ids)-2,'fullstack')
-		populatetemtable(mutationIds)
-	elif len(ind28Ids)==2:
-		pass
-	elif len(ind28Ids)==1:
-		for x in tempids[9:]:
-			prjno=IndConsidered()
-			prjno.indId=x
-			prjno.save()
-		mutationIds=getteamindividuals(1,'fullstack')
-		populatetemtable(mutationIds)
-	elif len(ind28Ids)==0:
-		for x in tempids[8:]:
-			prjno=IndConsidered()
-			prjno.indId=x
-			prjno.save()
-		mutationIds=getteamindividuals(2,'fullstack')
-		populatetemtable(mutationIds) 
-
+#def mutation():
+#	
+#	tempids = TempTeam.objects.values_list('indId',flat=True)
+#	ind28Ids = Individuals.objects.filter(Q(indGrade=28)).filter(indId__in=tempids).values_list('indId',flat=True)
+#	if len(ind28Ids)>2:
+#		for x in ind28Ids[2:]:
+#			prjno=IndConsidered()
+#			prjno.indId=x
+#			prjno.save()
+#		mutationIds=getteamindividuals(len(ind28Ids)-2,'fullstack')
+#		populatetemtable(mutationIds)
+#	elif len(ind28Ids)==2:
+#		pass
+#	elif len(ind28Ids)==1:
+#		for x in tempids[9:]:
+#			prjno=IndConsidered()
+#			prjno.indId=x
+#			prjno.save()
+#		mutationIds=getteamindividuals(1,'fullstack')
+#		populatetemtable(mutationIds)
+#	elif len(ind28Ids)==0:
+#		for x in tempids[8:]:
+#			prjno=IndConsidered()
+#			prjno.indId=x
+#			prjno.save()
+#		mutationIds=getteamindividuals(2,'fullstack')
+#		populatetemtable(mutationIds) 
+#
 			
 ########### Readfile to return file records as as list #####################
-def getValues(filename):
-	values = []
-	try:
-		file=open(filename,'r')
-		next(file)
-		for line in file:
-			values.append(line.replace('\n',''))
-		return values
-	except OSError as err:
-		print("OS error: {0}".format(err))
-	except ValueError:
-		print("Could not convert data to an integer.")
-	except:
-		print("Unexpected error:", sys.exc_info()[0])
-		raise
+#def getValues(filename):
+#	values = []
+#	try:
+#		file=open(filename,'r')
+#		next(file)
+#		for line in file:
+#			values.append(line.replace('\n',''))
+#		return values
+#	except OSError as err:
+#		print("OS error: {0}".format(err))
+#	except ValueError:
+#		print("Could not convert data to an integer.")
+#	except:
+#		print("Unexpected error:", sys.exc_info()[0])
+#		raise
 ########### Readfile to retunr file records as as list #####################
 def CreateInd(ind):
 	emp = Individuals()
@@ -130,72 +130,82 @@ def CreateInd(ind):
 	emp.save()
 
 ############ Get count of records for Individuals table ###############
-def countofrecords():
-	ct = Individuals.objects.count()
-	return ct
+#def countofrecords():
+#	ct = Individuals.objects.count()
+#	return ct
 
 ############ Populate Project table for fitness ###############
 
-def populatetemtable(tempteam):
-	idlist=[]
+#def populatetemtable(tempteam):
+#	idlist=[]
+#
+#	prjno=Prjnumbers()
+#	prjno.tag='prj'
+#	prjno.save()
+#	projectnumber = Prjnumbers.objects.values_list('prjID', flat=True).last()
+#
+#	for x in tempteam:
+#		idlist.append(x)
+#
+#	tdevopsRatio=gettdevopsRatio(idlist)
+#	tdesignRatio=gettdesignRatio(idlist)
+#	tavgCost=gettavgCost(idlist)
+#	tOnOffRatio=gettOnOffRatio(idlist)
+#	tratioGtAvgExp=gettratioGtAvgExp(idlist)
+#	tAvgNoOfPto=gettAvgNoOfPto(idlist)
+#	tAvgSkillLevel=gettAvgSkillLevel(idlist)
+#	tCntdistinctskills=gettCntdistinctskills(idlist)
+#	tFitnessValue= gettFitnessValue(tdevopsRatio,tdesignRatio,tavgCost,tOnOffRatio,\
+#								 tratioGtAvgExp,tAvgNoOfPto,tAvgSkillLevel,tCntdistinctskills,)
+#
+#
+#
+#	for x in tempteam:
+#		temp=TempTeam()
+#		temp.tname=projectnumber
+#		temp.indId= x
+#		temp.tdevopsRatio=tdevopsRatio
+#		temp.tdesignRatio= tdesignRatio
+#		temp.tavgCost=tavgCost['indCost__avg']
+#		temp.tOnOffRatio=  tOnOffRatio
+#		temp.tratioGtAvgExp=  tratioGtAvgExp['indExp__avg']
+#		temp.tAvgNoOfPto  = tAvgNoOfPto['indNoPto__avg']
+#		temp.tAvgSkillLevel  = tAvgSkillLevel['indSkillLevel__avg']
+#		temp.tCntdistinctskills  = tCntdistinctskills
+#		temp.tFitnessValue  = tFitnessValue 	
+#		temp.save()
 
-	prjno=Prjnumbers()
-	prjno.tag='prj'
-	prjno.save()
-	projectnumber = Prjnumbers.objects.values_list('prjID', flat=True).last()
-
-	for x in tempteam:
-		idlist.append(x)
-
-	tdevopsRatio=gettdevopsRatio(idlist)
-	tdesignRatio=gettdesignRatio(idlist)
-	tavgCost=gettavgCost(idlist)
-	tOnOffRatio=gettOnOffRatio(idlist)
-	tratioGtAvgExp=gettratioGtAvgExp(idlist)
-	tAvgNoOfPto=gettAvgNoOfPto(idlist)
-	tAvgSkillLevel=gettAvgSkillLevel(idlist)
-	tCntdistinctskills=gettCntdistinctskills(idlist)
-	tFitnessValue= gettFitnessValue(tdevopsRatio,tdesignRatio,tavgCost,tOnOffRatio,\
-								 tratioGtAvgExp,tAvgNoOfPto,tAvgSkillLevel,tCntdistinctskills,)
-
-
-
-	for x in tempteam:
-		temp=TempTeam()
-		temp.tname=projectnumber
-		temp.indId= x
-		temp.tdevopsRatio=tdevopsRatio
-		temp.tdesignRatio= tdesignRatio
-		temp.tavgCost=tavgCost['indCost__avg']
-		temp.tOnOffRatio=  tOnOffRatio
-		temp.tratioGtAvgExp=  tratioGtAvgExp['indExp__avg']
-		temp.tAvgNoOfPto  = tAvgNoOfPto['indNoPto__avg']
-		temp.tAvgSkillLevel  = tAvgSkillLevel['indSkillLevel__avg']
-		temp.tCntdistinctskills  = tCntdistinctskills
-		temp.tFitnessValue  = tFitnessValue 	
-		temp.save()
-
-def getPopularSkill(Id):
-	most_common = Individuals.objects.filter(indId__in=Id).filter(~Q(indRole='domain')).annotate(mc=Count('indSkill')).order_by('-mc')[:1]
-	mostcommonskill= most_common.values_list('indSkill',flat=True)
-	popularskillIds = Individuals.objects.filter(indId__in=Id).filter(indSkill__in=mostcommonskill).values_list('indId',flat=True)
-	return popularskillIds
+#def getPopularSkill(Id):
+#	most_common = Individuals.objects.filter(indId__in=Id).filter(~Q(indRole='domain')).annotate(mc=Count('indSkill')).order_by('-mc')[:1]
+#	mostcommonskill= most_common.values_list('indSkill',flat=True)
+#	popularskillIds = Individuals.objects.filter(indId__in=Id).filter(indSkill__in=mostcommonskill).values_list('indId',flat=True)
+#	return popularskillIds
 
 def gettdevopsRatio(Id):
-	return 100*Individuals.objects.filter(indRole='devops').filter(indId__in=Id).count()/Individuals.objects.filter(indId__in=Id).count()
-
+	try:
+		return 100*Individuals.objects.filter(indRole='devops').filter(indId__in=Id).count()/Individuals.objects.filter(indId__in=Id).count()
+	except:
+		return 0
 def gettdesignRatio(Id):
-	return 100*Individuals.objects.filter(indRole='design').filter(indId__in=Id).count()/Individuals.objects.filter(indId__in=Id).count()
-
+	try:
+		return 100*Individuals.objects.filter(indRole='design').filter(indId__in=Id).count()/Individuals.objects.filter(indId__in=Id).count()
+	except:
+		return 0
 def gettavgCost(Id):
-	return Individuals.objects.filter(indId__in=Id).aggregate(Avg('indCost'))
-
+	try:
+		return Individuals.objects.filter(indId__in=Id).aggregate(Avg('indCost'))
+	except:
+		return 0
 def gettOnOffRatio(Id):
-	return 100*Individuals.objects.filter(indRole='off').filter(indId__in=Id).count()/Individuals.objects.filter(indId__in=Id).count()
-
+	try:
+		return 100*Individuals.objects.filter(indRole='off').filter(indId__in=Id).count()/Individuals.objects.filter(indId__in=Id).count()
+	except:
+		return 0
 def gettratioGtAvgExp(Id):
-	return Individuals.objects.filter(indId__in=Id).aggregate(Avg('indExp'))
-
+	try:
+		return Individuals.objects.filter(indId__in=Id).aggregate(Avg('indExp'))
+	except:
+		return 0
 def gettAvgNoOfPto(Id):
 	return Individuals.objects.filter(indId__in=Id).aggregate(Avg('indNoPto'))
 
@@ -208,32 +218,34 @@ def gettCntdistinctskills(Id):
 def gettFitnessValue(tdevopsRatio,tdesignRatio,tavgCost,tOnOffRatio,\
 								 tratioGtAvgExp,tAvgNoOfPto,tAvgSkillLevel,tCntdistinctskills,):
 	i=0
-	if tdevopsRatio == 50:
-		i=i+1
-	if tdesignRatio == 30:
-		i=i+1
-	if tavgCost['indCost__avg'] >=60 and tavgCost['indCost__avg'] <=70:
-		i=i+1
-	if tOnOffRatio == 90:
-		i=i+1
-	if tratioGtAvgExp['indExp__avg'] >=4 and tratioGtAvgExp['indExp__avg']<=6:
-		i=i+1
-	if tAvgSkillLevel['indSkillLevel__avg'] >=2.5 and tAvgSkillLevel['indSkillLevel__avg'] <=5:
-		i=i+1
-	if tCntdistinctskills <=5:
-		i=i+1
-	
-	i= i/7
+	try:
+		if tdevopsRatio == 50:
+			i=i+1
+		if tdesignRatio == 30:
+			i=i+1
+		if tavgCost['indCost__avg'] >=60 and tavgCost['indCost__avg'] <=70:
+			i=i+1
+		if tOnOffRatio == 90:
+			i=i+1
+		if tratioGtAvgExp['indExp__avg'] >=4 and tratioGtAvgExp['indExp__avg']<=6:
+			i=i+1
+		if tAvgSkillLevel['indSkillLevel__avg'] >=2.5 and tAvgSkillLevel['indSkillLevel__avg'] <=5:
+			i=i+1
+		if tCntdistinctskills <=5:
+			i=i+1	
+		i= i/7
+		return str("{:%}".format(i))
+#		if(	tdevopsRatio == 50 and \
+#			tdesignRatio ==30 and \
+#			tavgCost['indCost__avg'] >=60 and tavgCost['indCost__avg'] <=70  and \
+#			tOnOffRatio == 90 and \
+#			tratioGtAvgExp['indExp__avg'] >=4 and tratioGtAvgExp['indExp__avg']<=6 and \
+#			tAvgNoOfPto['indNoPto__avg'] <= 20 and \
+#			tAvgSkillLevel['indSkillLevel__avg'] >=2.5 and tAvgSkillLevel['indSkillLevel__avg'] <=5 and \
+#			tCntdistinctskills == 1 ) :
+#			return "fit" + " : " + str("{:2f}".format(i))
+#		else :
+#			return "not fit" + " : " + str("{:%}".format(i))
 
-
-	if(	tdevopsRatio == 50 and \
-		tdesignRatio ==30 and \
-		tavgCost['indCost__avg'] >=60 and tavgCost['indCost__avg'] <=70  and \
-		tOnOffRatio == 90 and \
-		tratioGtAvgExp['indExp__avg'] >=4 and tratioGtAvgExp['indExp__avg']<=6 and \
-		tAvgNoOfPto['indNoPto__avg'] <= 20 and \
-		tAvgSkillLevel['indSkillLevel__avg'] >=2.5 and tAvgSkillLevel['indSkillLevel__avg'] <=5 and \
-		tCntdistinctskills == 1 ) :
-		return "fit" + " : " + str("{:2f}".format(i))
-	else :
-		return "not fit" + " : " + str("{:%}".format(i))
+	except:
+		return "not fit 0%"
